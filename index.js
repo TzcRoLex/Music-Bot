2ca07a2e7ca7a2b12428719a76c6c96110b13a52
TzcRoLex <tzcrolex22@gmail.com>
2022-12-10 20:09:43 +0200

    music-bot

---
+++ index.js
@@ -0,0 +1,107 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+
+const { DisTube } = require("distube");
+const { SpotifyPlugin } = require("@distube/spotify");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+const config = require('./config.js');
+
+client.distube = new DisTube(client, {
+  leaveOnFinish: true,
+  emitAddSongWhenCreatingQueue: false,
+  emitNewSongOnly: true,
+  plugins: [new SpotifyPlugin()]
+})
+
+client.commands = new Collection();
+client.emotes = config.emoji;
+
+const status = queue =>
+  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${
+    queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
+  }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
+client.distube
+  .on('playSong', (queue, song) =>
+    queue.textChannel.send(
+      `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
+        song.user
+      }\n${status(queue)}`
+    )
+  )
+  .on('addSong', (queue, song) =>
+    queue.textChannel.send(
+      `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    )
+  )
+  .on('addList', (queue, playlist) =>
+    queue.textChannel.send(
+      `${client.emotes.success} | Added \`${playlist.name}\` playlist (${
+        playlist.songs.length
+      } songs) to queue\n${status(queue)}`
+    )
+  )
+  .on('error', (channel, e) => {
+    if (channel) channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
+    else console.error(e)
+  })
+  .on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
+  .on('searchNoResult', (message, query) =>
+    message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
+  )
+  .on('finish', queue => queue.textChannel.send('Finished!'))
+
+module.exports = client;
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+require("./deploy-commands")
+
+// console.log(client)
+
+client.on("messageCreate", async (message) => {
+  if (message.content.length < 5) return message.delete();
+  let embed = new EmbedBuilder()
+  .setDescription(`${message.content}`)
+
+  let attachment = message.attachments.first() || "None";
+
+  if (attachment !== "None") {
+    embed.setImage(`${attachment.proxyURL}`)
+  }
+
+  message.delete();
+
+  await message.channel.send({ embeds: [embed] }).then(async (m) =>{
+    await m.react(":true:")
+    await m.react(":true:")
+  })
+})
+
+client.login(process.env.token);
